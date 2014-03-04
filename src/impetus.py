c6c135a79dca19e8e5c3f231ab75165db646231a
Richard J. Marini <richardjmarini@gmail.com>
2014-03-04 14:18:09 -0500

    changed to license version 2 or greater

--- src/impetus.py
+++ src/impetus.py
@@ -9,7 +9,7 @@
 # License:
 #    Impetus is free software: you can redistribute it and/or modify
 #    it under the terms of the GNU General Public License as published by
-#    the Free Software Foundation, either version 3 of the License, or
+#    the Free Software Foundation, either version 2 of the License, or
 #    any later version.
 #
 #    Impetus is distributed in the hope that it will be useful,
